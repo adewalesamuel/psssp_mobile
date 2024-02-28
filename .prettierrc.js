8b5c8969ac89318d3cc2b6d7989d65ac7607ab16
Samuel Adewale <samueladewale@192.168.1.24>
2024-02-28 10:44:16 +0000

    first commit

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,7 @@
 module.exports = {
-  arrowParens: 'avoid',
+  arrowParens: 'allow',
   bracketSameLine: true,
   bracketSpacing: false,
   singleQuote: true,
-  trailingComma: 'all',
+  trailingComma: 'never',
 };
